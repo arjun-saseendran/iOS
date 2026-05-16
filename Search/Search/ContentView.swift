4b9aecf83f577dfe592437a7310764d4a046367e
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-16 09:42:16 +0530

    fix search bar

--- Search/Search/ContentView.swift
+++ Search/Search/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var search: String = ""
+    @State private var friends: [String] = ["Maria", "Devika", "Aswini", "Manjima", "Sreekutty"]
+    @State private var filteredFriends: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+       VStack {
+            List(filteredFriends, id: \.self){ friend in
+                Text(friend)
+            }
+            .listStyle(.plain)
+            .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always))
+            .onChange(of: search){
+                if search.isEmpty {
+                    filteredFriends = friends
+                }else{
+                    filteredFriends = friends.filter {$0.contains(search)}
+                }
+               
+            }
+            
+           Spacer()
         }
+        
         .padding()
+        .onAppear(perform:{filteredFriends = friends})
+        .navigationTitle("Friends")
+        
+        
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack{
+        ContentView()
+    }
+    
 }
--- Search/Search/SearchApp.swift
+++ Search/Search/SearchApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct SearchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack{
+                ContentView()
+            }
         }
     }
 }
