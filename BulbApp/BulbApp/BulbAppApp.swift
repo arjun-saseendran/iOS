71821c228f073505b12a0d0f4d5e6d762b852628
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-16 10:28:11 +0530

    add bulb app

---
+++ BulbApp/BulbApp/BulbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulbAppApp.swift
+//  BulbApp
+//
+//  Created by Arjun Saseendran on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BulbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
