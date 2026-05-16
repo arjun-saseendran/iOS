8e9a9fa9c7506185031a5a648303ba5c1625a8cc
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-16 12:46:37 +0530

    add old method for pre ios 17

---
+++ GlobalStatePreIOS17/GlobalStatePreIOS17/GlobalStatePreIOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalStatePreIOS17App.swift
+//  GlobalStatePreIOS17
+//
+//  Created by Arjun Saseendran on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GlobalStatePreIOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
