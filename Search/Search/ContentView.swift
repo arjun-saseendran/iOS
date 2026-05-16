cd79c7b931116cf78f3a7f74a076a0afe8cc0e37
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-16 09:15:17 +0530

    add search

---
+++ Search/Search/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Search
+//
+//  Created by Arjun Saseendran on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Search/Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by Arjun Saseendran on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
